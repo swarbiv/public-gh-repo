73d00e5afa9c32019fe4858a3426cbd16eba0eb9
Scott W. Arbiv <swarbiv@ll.mit.edu>
2014-03-19 10:10:40 -0400

    moving things around and adding tests

--- test.js
+++
@@ -1,6 +0,0 @@
-var AES = require("crypto-js/aes");
-var SHA256 = require("crypto-js/sha256");
-console.log(SHA256("Message"));
-
-var CryptoJS = require("crypto-js");
-console.log(CryptoJS.HmacSHA1("Message", "Key"));
